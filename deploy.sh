7230fb4514fc5c5fbdc74a4ed368eba6ffbb9494
Nakul Bajaj <nakul.bajaj@anz.com>
2019-05-20 00:29:46 +1000

    changed deployment script to read environment variables properly

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t $DOCKER_ID/complex-clientapp:latest -t $DOCKER_ID/complex-clientapp:$GIT_SHA ./Complex/client
-docker build -t $DOCKER_ID/complex-server:latest -t $DOCKER_ID/complex-server:$GIT_SHA  ./Complex/server
-docker build -t $DOCKER_ID/complex-worker:latest -t $DOCKER_ID/complex-worker:$GIT_SHA  ./Complex/worker
-docker push $DOCKER_ID/complex-clientapp:latest
-docker push $DOCKER_ID/complex-server:latest
-docker push $DOCKER_ID/complex-worker:latest
-docker push $DOCKER_ID/complex-clientapp:$GIT_SHA
-docker push $DOCKER_ID/complex-server:$GIT_SHA
-docker push $DOCKER_ID/complex-worker:$GIT_SHA
+docker build -t $DOCKER_USERNAME/complex-clientapp:latest -t $DOCKER_USERNAME/complex-clientapp:$GIT_SHA ./Complex/client
+docker build -t $DOCKER_USERNAME/complex-server:latest -t $DOCKER_USERNAME/complex-server:$GIT_SHA  ./Complex/server
+docker build -t $DOCKER_USERNAME/complex-worker:latest -t $DOCKER_USERNAME/complex-worker:$GIT_SHA  ./Complex/worker
+docker push $DOCKER_USERNAME/complex-clientapp:latest
+docker push $DOCKER_USERNAME/complex-server:latest
+docker push $DOCKER_USERNAME/complex-worker:latest
+docker push $DOCKER_USERNAME/complex-clientapp:$GIT_SHA
+docker push $DOCKER_USERNAME/complex-server:$GIT_SHA
+docker push $DOCKER_USERNAME/complex-worker:$GIT_SHA
 kubectl apply -f ./Complex/k8s
-kubectl set image deployments/server-deployment server=$DOCKER_ID/complex-server:$GIT_SHA
-kubectl set image deployments/client-deployment client=$DOCKER_ID/complex-clientapp:$GIT_SHA
-kubectl set image deployments/worker-deployment worker=$DOCKER_ID/complex-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/complex-server:$GIT_SHA
+kubectl set image deployments/client-deployment client=$DOCKER_USERNAME/complex-clientapp:$GIT_SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_USERNAME/complex-worker:$GIT_SHA
