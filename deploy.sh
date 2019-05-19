849dd5b7a1024a3ae29ce9a2f861524a7a434075
Nakul Bajaj <nakul.bajaj@anz.com>
2019-05-20 00:22:42 +1000

    changed location of service account key

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,6 @@
-docker build -t nakul/complex-clientapp:latest ./Complex/client
-docker build -t nakul/complex-server:latest ./Complex/server
-docker build -t nakul/complex-worker:latest ./Complex/worker
-docker tag nakul/complex-clientapp $DOCKER_ID/complex-clientapp:latest
-docker tag nakul/complex-server $DOCKER_ID/complex-server:latest
-docker tag nakul/complex-worker $DOCKER_ID/complex-worker:latest
-docker tag nakul/complex-clientapp $DOCKER_ID/complex-clientapp:$GIT_SHA
-docker tag nakul/complex-server $DOCKER_ID/complex-server:$GIT_SHA
-docker tag nakul/complex-worker $DOCKER_ID/complex-worker:$GIT_SHA
+docker build -t $DOCKER_ID/complex-clientapp:latest -t $DOCKER_ID/complex-clientapp:$GIT_SHA ./Complex/client
+docker build -t $DOCKER_ID/complex-server:latest -t $DOCKER_ID/complex-server:$GIT_SHA  ./Complex/server
+docker build -t $DOCKER_ID/complex-worker:latest -t $DOCKER_ID/complex-worker:$GIT_SHA  ./Complex/worker
 docker push $DOCKER_ID/complex-clientapp:latest
 docker push $DOCKER_ID/complex-server:latest
 docker push $DOCKER_ID/complex-worker:latest
